a40f47b4a0340b1d418a881a9f631a98f3e6d0a7
Robin Ahlqvist <95robahl@gafe.molndal.se>
2023-10-24 14:53:16 +0200

    add exercise builder and property switches, add/remove button

--- wwwroot/js/planner.js
+++ wwwroot/js/planner.js
@@ -1,6 +1,5 @@
 const conditioningButton = document.getElementById("conditioning-button");
 const strengthButton = document.getElementById("strength-button");
-const intervalsButton = document.getElementById("intervals-button");
 
 const workoutCarousel = document.getElementById("workout-carousel");
 
@@ -17,7 +16,21 @@ function workoutOnClick(id) {
     }
 }
 
+function checkbox(boxId, lableId) {
+    const checkbox = document.getElementById(boxId);
+    const labelText = document.getElementById(lableId);
+    const exerciseBuilder = document.getElementById("exercise-builder");
+
+    if (checkbox.checked) {
+        labelText.textContent = "Remove Exercise";
+        exerciseBuilder.style.display = "contents";
+    } else {
+        labelText.textContent = "Add Exercise";
+        exerciseBuilder.style.display = "none";
+    }
+};
+
 
 document.getElementById("strength-button").addEventListener("click", () => workoutOnClick("strength-builder"));
 document.getElementById("conditioning-button").addEventListener("click", () => workoutOnClick("conditioning-builder"));
-document.getElementById("intervals-button").addEventListener("click", () => workoutOnClick("intervals-builder"));
+document.getElementById("btn-check-outlined").addEventListener("change", () => checkbox("btn-check-outlined", "label-text"));
